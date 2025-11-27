37e112858960484cf15eace0ca642d2026df6ea2
Ashwin Naren <arihant2math@gmail.com>
2025-11-27 11:39:08 -0800

    implement fair scheduler

--- src/process/clone.rs
+++ src/process/clone.rs
@@ -124,6 +124,9 @@ pub async fn sys_clone(
             priority: current_task.priority,
             sig_mask: SpinLock::new(new_sigmask),
             pending_signals: SpinLock::new(SigSet::empty()),
+            vruntime: SpinLock::new(*current_task.vruntime.lock_save_irq()),
+            exec_start: SpinLock::new(None),
+            deadline: SpinLock::new(*current_task.deadline.lock_save_irq()),
             state: Arc::new(SpinLock::new(TaskState::Runnable)),
             last_run: SpinLock::new(None),
         }
